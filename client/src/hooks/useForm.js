7addf24d5b792efccb225fb92b397d326f8d6111
Tony Sorensen <tony48853@gmail.com>
2020-09-12 10:08:22 -0700

    useForm hook built and controlling state. able to checkout from cart and recieve success message

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,18 @@
 // write your custom hook here to control your checkout form
+import { useState } from "react";
+
+export const useForm = (key, initialValue) => {
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+  const [values, setValues] = useState(key, initialValue);
+
+  const handleChanges = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setShowSuccessMessage(true);
+  };
+
+  return [values, showSuccessMessage, handleChanges, handleSubmit];
+};
